ae8785ac2e76e66d749e766155bde02f494fc734
Bruno Germano <bruno.germano@gmail.com>
2020-09-12 11:00:59 -0300

    setup gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jornada`,
+    description: `Criando jornadas de aprendizados de ciências da computação e programação.`,
+    author: `@egermano`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
