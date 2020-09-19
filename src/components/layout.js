f38781e7cf568e1184fe705f28804c6f1a7f300b
Bruno Germano <bruno.germano@gmail.com>
2020-09-19 12:06:40 -0300

    update main layout
    - remove inline css
    - add className prop
    - add default className

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,7 +13,7 @@ import Header from "./header"
 import Footer from "./footer"
 import "../style/layout.scss"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, className }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -28,11 +28,7 @@ const Layout = ({ children }) => {
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
+        className={`page ${className}`}
       >
         <main>{children}</main>
         <Footer></Footer>
