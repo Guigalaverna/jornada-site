a3cb9b361e86591de9ce1616d2893bd6d05eca7d
Bruno Germano <bruno.germano@gmail.com>
2021-01-04 18:19:36 -0300

    DK

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import SignupForm from "../components/signupForm"
+import SignupForm from "./sections/index/signupForm"
 
 // Sections
 import SectionAbout from "./sections/index/about"
@@ -10,7 +10,7 @@ import SectionHero from "./sections/index/hero"
 
 const IndexPage = () => {
   const [email, setEmail] = useState("")
-  const onSubmit = data => setEmail(data)
+  const onSubmit = e => setEmail(e) // email from component
 
   return (
     <Layout className="home">
